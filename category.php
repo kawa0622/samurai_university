5e36792b552377b38c993edcfa936d793ec81a07
kawa0622 <fmhcf972@yahoo.co.jp>
2025-04-16 21:01:29 +0900

    ループ処理で投稿を取得・表示する

--- category.php
+++ category.php
@@ -28,36 +28,51 @@
                 <div class="tab_panel_content">
                   <div class="tab_panel_text">
                     <!-- news loop from here-->
+                     <?php if (have_posts()) : ?>
+                      <?php while(have_post()) : the_post(); ?>
                     <div class="news_posts_small">
                       <div class="row">
                         <div class="col-lg-2 col-md-2 col-sx-12">
                           <div class="calendar_news_border">
                             <div class="calendar_news_border_1">
-                              <div class="calendar_month">OCT</div>
+                              <div class="calendar_month">
+                                <?php echo get_post_time('F'); ?>
+                              </div>
                               <div class="calendar_day">
-                                <span>15</span>
+                                <span>
+                                  <?php echo get_post_time('d'); ?>
+                                </span>
                               </div>
                             </div>
                           </div>
+                          <?php
+                          if (is_category('event')) :// イベントのカテゴリーの場合
+                            ?>
+                            <div class="calender_hour"><?php echo post_costom('time'); ?>
+                            <?php
+                            endif;
+                            ?>
                         </div>
                         <div class="col-lg-10 col-md-10 col-sx-12">
                           <div class="news_post_small_title">
-                            <a href="news_detail.html">AWSハンズオンセミナー</a>
+                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                           </div>
                           <div class="news_post_meta">
                             <ul>
                               <li>
-                                10月8日　新宿〇〇ビル8Fにて、AWSハンズオンセミナーを開催します。AWSを実際に構築してみます。参加希望者は...
+                               <?php echo wp_trim_words( get_the_content() , 100, '...'); ?>
                               </li>
                             </ul>
                           </div>
                           <div class="read_continue">
-                            <button onclick="location.href='news_detail.html'">詳細を見る</button>
+                            <button> <a href="<?php the_permalink(); ?>" class="text-white">詳細を見る</a></button>
                           </div>
                         </div>
                       </div>
                       <hr />
                     </div>
+                    <?php endwhile; ?>
+                    <?php endif; ?>
                     <!-- news loop until here-->
 
                     <div class="news_posts_small">
